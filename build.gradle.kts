b3e5c7c2beaf2a701fb8ea53ae69f662ec81e690
Rémi Bleuse <remi.bleuse@gmail.com>
2024-03-13 20:31:41 +0900

    use beta version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "1.9.23"
+	val kotlinVersion = "2.0.0-Beta4"
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version kotlinVersion
