1f86e71802ea45bb81340e6a45dfd2487bf53ab1
Rémi Bleuse <remi.bleuse@gmail.com>
2024-04-13 15:07:12 +0900

    Update UserRepositoryTest.kt

--- src/test/kotlin/com/rbleuse/redditclonekotlin/repository/UserRepositoryTest.kt
+++ src/test/kotlin/com/rbleuse/redditclonekotlin/repository/UserRepositoryTest.kt
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
+@ActiveProfiles("test")
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
